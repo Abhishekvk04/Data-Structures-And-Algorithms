26a58ef376a834cfde6e7f1ccc1b11ef2f174c99
Abhishek V K <117388178+Abhishekvk04@users.noreply.github.com>
2023-03-22 13:48:05 +0530

    program to convert lower case string to uppercase using built in functions

---
+++ String Problems/Uppercase_convert.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+char str1[10];
+int i; 
+printf(“Enter the string\n”);
+gets(str1);
+strupr(str1)
+printf(“string in upper case is  “);
+puts(str1);
+}
