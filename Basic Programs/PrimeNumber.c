e2f165e08ef47a90b8828a21e38850568c7af700
Abhishek V K <117388178+Abhishekvk04@users.noreply.github.com>
2024-01-26 01:46:18 +0530

    Update PrimeNumber.c

--- Basic Programs/PrimeNumber.c
+++ Basic Programs/PrimeNumber.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
+#include<math.h>
+
 int main(){
-  int num, i, flag= 1;
+  int num, i, flag= 1, inum;
   printf("Enter a positive number\n");
   scanf("%d",&num);
-  for (i = 2; i < num ; i++){
+
+  //If the value of num = 41
+  //inum = num; -> for loop iterates 40 times
+  //inum = num / 2; -> Here the for loop iterates 40/2=20 i.e., 19 times
+  inum = sqrt(num); // Here the for loop iterates only 6 times which is an optimized solution
+  
+  for (i = 2; i <= inum; i++){
     //<=n/2 or i<=sqrt(n)
     if (num % i == 0 )
       flag = 0;
@@ -12,5 +20,5 @@ int main(){
 if (flag)
       printf("It is a prime number\n"); 
     else
-      printf("It is a pnot prime number\n");
+      printf("It is not a prime number\n");
 }
